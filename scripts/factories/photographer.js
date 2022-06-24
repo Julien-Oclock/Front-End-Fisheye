89c77105dc21537cc8aa31f7505c5925ae1d0510
Julien-Oclock <julien.lxpro@gmail.com>
2022-06-24 09:49:04 +0200

    connect data / update factory

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,5 +1,5 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, tagline, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
@@ -9,9 +9,20 @@ function photographerFactory(data) {
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+        const h3 = document.createElement( 'h3' );
+        h3.textContent = `${city}, ${country}`;
+        const p = document.createElement( 'p' );
+        p.textContent = tagline;
+        const priceTag = document.createElement( 'span' );
+        priceTag.textContent = `${price}€/jour`;
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(h3);
+        article.appendChild(p);
+        article.appendChild(priceTag);
         return (article);
     }
     return { name, picture, getUserCardDOM }
 }
+
+export default photographerFactory;
