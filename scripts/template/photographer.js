602bbfe7b1ec6d6d54d3a926fe3f1cf6e767e9fd
julien Levieux <julien.levieux@drotek.com>
2022-06-30 14:15:00 +0200

    add some accessibility attribute

--- scripts/template/photographer.js
+++ scripts/template/photographer.js
@@ -4,14 +4,14 @@ export default function photographerTemplate(data){
 
     return (
         `
-            <a href=../photographer.html?id=${data.id} class="photographer__link" aria-label="aller à la page de ${data.name}" role="link">
+            <a href="../photographer.html?id=${data.id}" class="photographer__link" aria-label="aller à la page de ${data.name}" role="link">
                 <img class="photographer__img" src=${picture} alt="portrait du photographe ${data.name}">
                 <h2 class="photographer__name">${data.name}</h2>   
-                <div id="${data.name}" class="photographer__data-container" role="informations du photographe ${data.name}">
+                <section id="${data.name}" class="photographer__data-container" role="informations du photographe ${data.name}">
                     <span aria-describedby="${data._name}" class="photographer__location">${data.city}, ${data.country}</span>
                     <span aria-describedby="${data.name}" class="photographer__tagline">${data.tagline}</span>
                     <span aria-describedby="${data.name}" class="photographer__price">${data.price} /jour</span>
-                </div>
+                </section>
             </a>
         `
     )
--- scripts/template/photographerDetails.js
+++ scripts/template/photographerDetails.js
@@ -1,17 +1,24 @@
 export default function photographerDetailsTemplate (data) {
     return (
         `
-            <div class="photographer-details">
+            <section class="photographer-details" role="informations du photographe ${data.name}">
                 <h2 class="photographer-details__name"> ${data.name}</h2>
                 <p class="photographer-details__location">${data.city}, ${data.country}</p>
                 <p class="photographer-details__tagline">${data.tagline}</p>
-            </div>
-            <div class="contact">
-                <button class="contact_button" onclick="displayModal()">Contactez-moi</button>
-            </div>
-            <div class="photographer-picture">
-                <img class="photographer__img" src="${data.path}" alt="portrait du photographe ${data.name}">
-            </div>
+            </section>
+            <section class="contact">
+                <button role="bouton" aria-label="ouvrir le formulaire de contact" class="contact_button" onclick="displayModal()">Contactez-moi</button>
+            </section>
+            <section class="photographer-picture" aria-label="photo de profil du photographe ${data.name}">
+                <img class="photographer-picture__img" src="${data.path}" alt="portrait du photographe ${data.name}">
+            </section>
+            <section class="card" aria-label=" tarif du photographe et nombres de like">
+                <p aria_label="nombres de likes" class="card__likes">
+                    122824
+                    <i class="fa-solid fa-heart"></i>
+                </p>
+                <p aria-label="prix à la journée" class="card__price">${data.price}&#x20AC;/jour</p>
+            </section>
             
         `
     )
