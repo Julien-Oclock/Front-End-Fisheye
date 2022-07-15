fcb0423cee0f50e9793d8ca2443742563d5be19c
Julien-Oclock <julien.lxpro@gmail.com>
2022-07-15 10:40:34 +0200

    handle form

--- scripts/template/modal.js
+++ scripts/template/modal.js
@@ -6,12 +6,16 @@ export default function modal(photographer){
             <h2 id="modalTitle">Contactez moi ${photographer.name}</h2>  
             <form method="post">
                 <label class="modal__label" for="firstname">Prénom</label>
+                <span class="modal__error" id="firstname_error"></span>
                 <input class="modal__input" type="text" id="firstname" name="firstname" placeholder="Votre Prénom" required>
                 <label class="modal__label" for="lastname">Nom</label>
+                <span class="modal__error" id="lastname_error"></span>
                 <input class="modal__input" type="text" id="lastname" name="lastname" placeholder="Votre nom" required>
                 <label class="modal__label" for="email">Email</label>
+                <span class="modal__error" id="email_error"></span>
                 <input class="modal__input" type="email" id="email" name="email" placeholder="Votre email" required>
                 <label class="modal__label" for="message">Message</label>
+                <span class="modal__error" id="message_error"></span>
                 <textarea class="modal__text-area" id="message" name="message" placeholder="Votre message" required></textarea>
                 <input class="modal__submit" type="submit" value="Envoyer">
             </form>
@@ -19,3 +23,4 @@ export default function modal(photographer){
         `
     )
 }
+
--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -8,3 +8,101 @@ function closeModal() {
     modal.style.display = "none";
 }
 
+
+
+const form = document.getElementById("contact_form");
+const firstName = document.querySelector("#firstname");
+const lastName = document.getElementById("lastname");
+const email = document.getElementById("email");
+const message = document.getElementById("message");
+const submit = document.getElementsByClassName("modal__submit")
+
+const regexEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
+const errorMessage = {
+    name:"Votre nom/prénom doit comporter au moins 2 characters",
+    mail:"Le format du mail n'est pas valide",
+    message:"Votre message doit comporter au moins 10 characters"
+};
+
+/****
+ * ************************************************************************************************
+ * FONCTION DE CHECK DES DONNEES SAISIE
+ * ************************************************************************************************
+ */
+
+//Check si la string est supérieur ou égale à 2 charactère
+//Si string.length est supérieur ou egale à 2 charactère return true
+//Sinon return false
+const validName = (string) => {
+    if ((string.length >=2) && (string.length)){
+      return true
+    } else false
+  };
+
+//Check si la string est supérieur ou égale à 10 charactère
+const checkMessage = (string) => {
+    if ((string.length >=10) && (string.length)){
+        return true
+    } else false
+}
+
+
+
+
+//Check si la string est un email valide
+const validEmail = (string) => {
+    if (regexEmail.test(string)){
+        return true
+    } else false
+}
+
+
+const handleFirstnameInput = (event) =>{
+    if (!validName(event.target.value)){
+        firstName.style.border = '3px solid #fe142f';
+        document.querySelector('.error-firstname').innerHTML = errorMessage.name;
+        console.log('false')
+    }else {
+        firstName.style.border = 'none';
+        document.querySelector('.error-firstname').innerHTML = "";
+    }
+}
+
+const handleLastnameInput = (event) =>{ 
+    if (!validName(event.target.value)) {
+        lastName.style.border = '3px solid #fe142f';
+        document.querySelector('.error-lastname').innerHTML = errorMessage.name;
+        console.log('false')
+    } else {
+        lastName.style.border = 'none';
+        document.querySelector('.error-lastname').innerHTML = "";
+    }
+}
+
+const handleMailInput = (event) => {
+    if (!regexEmail.test(event.target.value)) {
+        mail.style.border = '3px solid #fe142f';
+        document.querySelector('.error-mail').innerHTML = errorMessage.mail;
+    } else {
+        mail.style.border = 'none';
+        document.querySelector('.error-mail').innerHTML = "";
+    }; 
+}
+
+const handleMessageInput = (event) => {
+    if (!checkMessage(event.target.value)) {
+        message.style.border = '3px solid #fe142f';
+        document.querySelector('.error-message').innerHTML = errorMessage.message;
+    } else {
+        message.style.border = 'none';
+        document.querySelector('.error-message').innerHTML = "";
+    }
+}
+
+console.log(firstName)
+
+firstName.addEventListener('change', handleFirstnameInput);
+lastName.addEventListener('change', handleLastnameInput);
+email.addEventListener('change', handleMailInput);  
+message.addEventListener('change', handleMessageInput);
