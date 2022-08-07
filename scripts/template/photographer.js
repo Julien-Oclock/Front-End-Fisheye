b8a08c983c6a350741d43a607c2de9747ab76c84
Julien-Oclock <julien.lxpro@gmail.com>
2022-08-07 16:11:36 +0200

    fix fetch url

--- scripts/template/photographer.js
+++ scripts/template/photographer.js
@@ -4,7 +4,7 @@ export default function photographerTemplate(data){
 
     return (
         `
-            <a href="../photographer.html?id=${data.id}" class="photographer__link" tabindex="0">
+            <a href="./photographer.html?id=${data.id}" class="photographer__link" tabindex="0">
                 <img class="photographer__img" src=${picture} tabindex="0">
                 <h2 class="photographer__name"  tabindex="0">${data.name}</h2>   
                 <section id="${data.name}" class="photographer__data-container">
