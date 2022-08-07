13ede9603e526427754eaa957d7929ba79865563
Julien-Oclock <julien.lxpro@gmail.com>
2022-08-07 15:55:06 +0200

    fix fetch url

--- scripts/template/photographer.js
+++ scripts/template/photographer.js
@@ -4,13 +4,13 @@ export default function photographerTemplate(data){
 
     return (
         `
-            <a href="../photographer.html?id=${data.id}" class="photographer__link" aria-label="link to ${data.name} pages" role="link" tabindex="0">
-                <img class="photographer__img" src=${picture} alt="profil picture of ${data.name}" tabindex="0">
-                <h2 class="photographer__name">${data.name}</h2>   
-                <section id="${data.name}" class="photographer__data-container" role="photographer info of ${data.name}">
-                    <span aria-describedby="${data._name}" aria-label="Photographer location" class="photographer__location" tabindex="0">${data.city}, ${data.country}</span>
-                    <span aria-describedby="${data.name}" aria-label="Photographer tagline" class="photographer__tagline" tabindex="0">${data.tagline}</span>
-                    <span aria-describedby="${data.name}" aria-label="Photographer price" class="photographer__price" tabindex="0">${data.price}Є/jour</span>
+            <a href="../photographer.html?id=${data.id}" class="photographer__link" tabindex="0">
+                <img class="photographer__img" src=${picture} tabindex="0">
+                <h2 class="photographer__name"  tabindex="0">${data.name}</h2>   
+                <section id="${data.name}" class="photographer__data-container">
+                    <p location" class="photographer__location" tabindex="0">${data.city}, ${data.country}</p>
+                    <p class="photographer__tagline" tabindex="0">${data.tagline}</p>
+                    <aside class="photographer__price" tabindex="0">${data.price}Є/jour</aside>
                 </section>
             </a>
         `
--- scripts/template/photographerDetails.js
+++ scripts/template/photographerDetails.js
@@ -1,16 +1,16 @@
 export default function photographerDetailsTemplate(data) {
     return (
         `
-            <section class="photographer-details" role="photographer banner" aria-label="informations du photographe ${data.name}">
-                <h2 class="photographer-details__name"  aria-label="photographer name"  > ${data.name}</h2>
-                <p class="photographer-details__location" aria-label="photographer location" tabindex="0">${data.city}, ${data.country} </p>
-                <p class="photographer-details__tagline"  aria-label="photographer tagline" tabindex="0"    >${data.tagline}</p>
+            <section class="photographer-details">
+                <h2 class="photographer-details__name"  tabindex="0"> ${data.name}</h2>
+                <p class="photographer-details__location" tabindex="0">${data.city}, ${data.country} </p>
+                <p class="photographer-details__tagline" tabindex="0">${data.tagline}</p>
             </section>
             <section class="contact">
-                <button role="bouton" aria-label="open contact modal" id="contact" class="contact_button" onclick="displayModal()">Contactez-moi</button>
+                <button role="bouton" aria-label="ouvrir le formulaire de contact" id="contact" class="contact_button" onclick="displayModal()">Contactez-moi</button>
             </section>
             <section class="photographer-picture">
-                <img class="photographer-picture__img" src="${data.path}" alt="profile picture of ${data.name}">
+                <img class="photographer-picture__img" src="${data.path}" alt="portrait ${data.name}">
             </section> 
         `
     )
