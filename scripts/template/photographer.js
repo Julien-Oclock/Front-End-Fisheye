893f935d970974bb50b51374f764330df9a98725
julien Levieux <julien.levieux@drotek.com>
2022-08-08 11:58:55 +0200

    fix error report from WAVE

--- scripts/template/photographer.js
+++ scripts/template/photographer.js
@@ -5,7 +5,7 @@ export default function photographerTemplate(data) {
     return (
         `
             <a href="./photographer.html?id=${data.id}" class="photographer__link" tabindex="0">
-                <img class="photographer__img" alt="profil picture" src=${picture} tabindex="0">
+                <img class="photographer__img" alt="profil picture of ${data.name}" src=${picture} tabindex="0">
                 <h2 class="photographer__name"  tabindex="0">${data.name}</h2>   
                 <section id="${data.name}" class="photographer__data-container">
                     <p location" class="photographer__location" tabindex="0">${data.city}, ${data.country}</p>
